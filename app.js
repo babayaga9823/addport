5de8def2e4afbb987c253348f0c17a0c81b41ca7
babayaga9823 <babayaga9823@gmail.com>
2020-05-15 21:57:52 +0530

    heroku🚀🚀

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("Hello")
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+// Define paths for Express config
+const publicDirectoryPath = './'
+
+// Setup handlebars engine and views location
+
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.send('index')
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Andrew Mead',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
